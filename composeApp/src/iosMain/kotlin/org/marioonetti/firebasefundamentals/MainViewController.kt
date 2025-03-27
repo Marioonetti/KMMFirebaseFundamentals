95a6191111d94c65c8942b782fee90caaa75221f
Mario Onetti <mario.onetti@worldline.com>
2025-03-27 10:24:34 +0100

    Auth working on ios

--- composeApp/src/iosMain/kotlin/org/marioonetti/firebasefundamentals/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/marioonetti/firebasefundamentals/MainViewController.kt
@@ -3,12 +3,14 @@ package org.marioonetti.firebasefundamentals
 import androidx.compose.ui.window.ComposeUIViewController
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
+import org.marioonetti.firebasefundamentals.data.di.dataModule
+import org.marioonetti.firebasefundamentals.domain.di.domainModule
 import org.marioonetti.firebasefundamentals.ui.di.uiModule
 
 fun MainViewController() = ComposeUIViewController {
     stopKoin()
     startKoin {
-        modules(uiModule)
+        modules(uiModule, domainModule, dataModule)
     }
     App()
 }
